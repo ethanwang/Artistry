5263a1bc6b11a2db75a2f4a15a7fad64314aeba9
wangkepeng <wangkepengwkp@hotmail.com>
2016-11-23 20:35:01 +0800

    first time

---
+++ Artistry/ArtistTableViewCell.swift
@@ -0,0 +1,18 @@
+//
+//  ArtistTableViewCell.swift
+//  Artistry
+//
+//  Created by 王轲鹏 on 2016/11/23.
+//  Copyright © 2016年 RayWenderlich. All rights reserved.
+//
+
+import UIKit
+
+class ArtistTableViewCell: UITableViewCell{
+  
+  @IBOutlet weak var bioLable: UILabel!
+  @IBOutlet weak var nameLabel: UILabel!
+  @IBOutlet weak var artistImageView: UIImageView!
+
+  
+}
---
+++ Artistry/WorkTableViewCell.swift
@@ -0,0 +1,17 @@
+//
+//  WorkTableViewCell.swift
+//  Artistry
+//
+//  Created by 王轲鹏 on 2016/11/23.
+//  Copyright © 2016年 RayWenderlich. All rights reserved.
+//
+
+import UIKit
+
+class WorkTableViewCell:UITableViewCell{
+  @IBOutlet weak var workImageView: UIImageView!
+  @IBOutlet weak var workTitleLabel: UILabel!
+  @IBOutlet weak var moreInfoTextView: UITextView!
+
+  
+}
